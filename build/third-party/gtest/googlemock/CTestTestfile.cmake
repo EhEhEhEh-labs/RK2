49abcdce61e916089386987577c4888d89308d23
EhEhEhEh-labs <new3now@yandex.ru>
2025-05-23 06:30:05 +0000

    PLEASE WORK

--- build/third-party/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/projects/rk2/third-party/gtest/googlemock
-# Build directory: /home/vboxuser/projects/rk2/build/third-party/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/third-party/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/projects/rk2/third-party/gtest/googletest
-# Build directory: /home/vboxuser/projects/rk2/build/third-party/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
